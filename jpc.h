13677a61d1dd9e0aa2219c48e49dccb7c7ee92ea
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2024-01-05 23:34:30 +0100

    Initial Commitc

---
+++ jpc.h
@@ -0,0 +1,26 @@
+/* C but in Japanese */
+
+#define 返しは return
+#define 循環 for
+
+#define 整数 int
+
+#define 主なる main
+#define プリントエフ printf
+
+#define 改行 "\n"
+
+#define 左小括弧 (
+#define 括弧閉じ )
+#define です ;
+
+#define 零 0
+#define 一 1
+#define 二 2
+#define 三 3
+#define 四 4
+#define 五 5
+#define 六 6
+#define 七 7
+#define 八 8
+#define 九 9
